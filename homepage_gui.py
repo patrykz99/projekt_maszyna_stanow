369d0a0006d0aff6e622874a47c690ef38361007
Szymon Urzedowski <szymon.urzedowski@pl.abb.com>
2023-06-16 01:49:48 +0200

    testing

--- homepage_gui.py
+++ homepage_gui.py
@@ -44,15 +44,20 @@ class GUI():
         
         self.price_label = customtkinter.CTkLabel(self.ticket_machine, text="Do zapłaty: "+str(cena)+"zł", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
         self.price_label.grid(row=1, column=0, columnspan=2, pady=10)
-        
+
+        #label that shows inserted coins value and updates it everytime a coin is inserted
+        self.inserted_coins_label = customtkinter.CTkLabel(self.ticket_machine, text="Wrzucono: 0zł", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
+        self.inserted_coins_label.grid(row=2, column=0, columnspan=2, pady=10)
+
         self.button_cancel = customtkinter.CTkButton(self.ticket_machine, text="Anuluj", height=50, text_color="#ffffff", hover_color="#c60b0b", corner_radius=7, fg_color="#000000", bg_color="#92a3f6",
                                              command=self.cancel_purchase)
-        self.button_cancel.grid(row=2, column=0,columnspan=2)
+        self.button_cancel.grid(row=3, column=0,columnspan=2)
         
         price = cena  # Cena biletu
         #obsluga monet
         result = self.sum_monety(price)
         if result[2] == True:
+            # reszta is result[1] rounded to 2 decimal places
             reszta = result[1]
             self.print_ticket(reszta)
 
@@ -61,14 +66,21 @@ class GUI():
     def sum_monety(self,price):
         sum = 0
         while True:
-            num = int(input("Wrzuc monete: "))
-            sum += num
-            if sum >= price:
-                if sum > price:
-                    print(f'Wrzucona ilosc: {sum}, Reszta do wydania: {sum - price}')
-                return sum, sum - price, True
+            num = float(input("Wrzuc monete: "))
+            #input must be one of 0.20, 0.50, 1, 2, 5, 10, 20, 50, 100, 200
+            if num in [0.20, 0.50, 1, 2, 5, 10, 20, 50, 100, 200]: 
+                sum += num
+                # everytime a coin is inserted, the label is updated
+                self.inserted_coins_label.configure(text="Wrzucono: "+str(sum)+"zł")
+                if sum >= price:
+                    if sum > price:
+                        reszta = format(sum - price, ".2f")
+                        print(f'Wrzucona ilosc: {sum}, Reszta do wydania: {reszta}')
+                    return sum, reszta, True
+                else:
+                    print(f'Pozostało do wrzucenia: {format(price - sum, ".2f")} zł')
             else:
-                print(f'Pozostało do wrzucenia: {price-sum} zł')
+                print("Nieprawidłowa moneta")           
 
 
     def cancel_purchase(self):
@@ -101,10 +113,10 @@ class GUI():
         self.selection_label.grid(row=2, column=0, columnspan=2, pady=10)
 
         #wait for 3 seconds before switching to recipt window
-        self.ticket_machine.after(3000, self.paragon)
+        self.ticket_machine.after(3000, self.paragon(reszta))
 
 
-    def paragon(self):
+    def paragon(self, reszta):
         #new window asking whether user wants a receipt
         self.clear_window()
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Czy chcesz paragon?", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
@@ -112,12 +124,12 @@ class GUI():
 
         #button to print receipt
         self.button_yes = customtkinter.CTkButton(self.ticket_machine, text="Tak", height=50, text_color="#ffffff", hover_color="#c60b0b", corner_radius=7, fg_color="#000000", bg_color="#92a3f6",
-                                                command=self.print_receipt)
+                                                command=lambda: self.print_receipt(reszta))
         self.button_yes.grid(row=1, column=0, pady=10)
 
         #button to not print receipt
         self.button_no = customtkinter.CTkButton(self.ticket_machine, text="Nie", height=50, text_color="#ffffff", hover_color="#c60b0b", corner_radius=7, fg_color="#000000", bg_color="#92a3f6",
-                                                command=self.no_receipt)
+                                                command=lambda: self.no_receipt(reszta))
         self.button_no.grid(row=1, column=1, pady=10)
 
 
@@ -129,24 +141,28 @@ class GUI():
     def display(self):
         return self.ticket_machine.mainloop()
     
-    def print_receipt(self):
+    def print_receipt(self, reszta):
         self.clear_window()
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Paragon został wydrukowany", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
         self.selection_label.grid(row=0, column=0, columnspan=2, pady=20)
         
+        #label with amount of change
+        self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Reszta: "+str(self.reszta)+"zł", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
+        self.selection_label.grid(row=1, column=0, columnspan=2, pady=20)
+
         #dodanie ze po 5 sekundach sie zamyka program
         self.ticket_machine.after(5000, self.ticket_machine.destroy)
 
-    def no_receipt(self):
+    def no_receipt(self, reszta):
         self.clear_window()
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Paragon nie został wydrukowany", width=300, height=50, font=("Arial",16), corner_radius=10, text_color="#000000")
         self.selection_label.grid(row=0, column=0, columnspan=2, pady=20)
         
         #dodanie ze po 5 sekundach sie zamyka program
         self.ticket_machine.after(5000, self.ticket_machine.destroy)
-        
 
 
-
-
+if __name__ == "__main__": 
     
+    gui = GUI()
+    gui.display()
