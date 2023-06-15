808a3b8df03c4352a24e503d89e5c4a77b82da8b
Szymon Urzedowski <szymon.urzedowski@pl.abb.com>
2023-06-15 23:52:38 +0200

    reszta

--- func.py
+++ func.py
@@ -1,5 +1,5 @@
 # function that keeps track of the sum of all the numbers that have been passed to it, and returns sum after exceding passed value
-# and the difference of the sum and the passed value, and boolkean value if the sum is greater than the passed value
+# and the difference of the sum and the passed value, and boolean value if the sum is greater than the passed value
 
 def sum(price):
     sum = 0
--- homepage_gui.py
+++ homepage_gui.py
@@ -1,70 +1,6 @@
 import customtkinter
-
-
-class GUI:
-    def __init__(self):
-        self.ticket_machine = customtkinter.CTk()
-        self.ticket_machine.title("Ticket Machine APP")
-        self.ticket_machine.geometry("400x300")  # wymiary okna
-        self.ticket_machine._set_appearance_mode("light")
-
-        # siatka 2x2
-        self.ticket_machine.grid_rowconfigure((0, 1), weight=1)
-        self.ticket_machine.grid_columnconfigure((0, 1), weight=1)
-
-    def display(self):
-        return self.ticket_machine.mainloop()
-
-    def create_buttons(self):
-        # przycis bilet normalny
-        button_norm = customtkinter.CTkButton(
-            self.ticket_machine,
-            text="Bilet normalny",
-            height=50,
-            text_color="#000000",
-            hover_color="#0c6d09",
-            corner_radius=7,
-            fg_color="#ffffff",
-            bg_color="#92a3f6",
-        )
-        button_norm.grid(row=1, column=0)
-        # przycisk bilet ulgowys
-        button_ulg = customtkinter.CTkButton(
-            self.ticket_machine,
-            text="Bilet ulgowy",
-            height=50,
-            text_color="#ffffff",
-            hover_color="#0c6d09",
-            corner_radius=7,
-            fg_color="#000000",
-            bg_color="#92a3f6",
-        )
-        button_ulg.grid(row=1, column=1)
-
-    def create_frames(self):
-        # top frame with text
-        top_frame = customtkinter.CTkFrame(
-            self.ticket_machine, corner_radius=0, fg_color="#92a3f6"
-        )
-        top_frame.grid(row=0, column=0, columnspan=2, sticky="nsew")
-        text = customtkinter.CTkLabel(
-            self.ticket_machine,
-            text="Wybierz bilet",
-            width=300,
-            height=50,
-            font=("Arial", 16),
-            corner_radius=10,
-            text_color="black",
-        )
-        text.grid(row=0, column=0, columnspan=2)
-        # bottom frame contains buttons
-        bottom_frame = customtkinter.CTkFrame(
-            self.ticket_machine, corner_radius=0, fg_color="#92a3f6"
-        )
-        bottom_frame.grid(row=1, column=0, columnspan=2, sticky="nsew")
 import customtkinter, time
 import tkinter as tk
-
 
 class GUI():
     def __init__(self):
@@ -117,7 +53,8 @@ class GUI():
         #obsluga monet
         result = self.sum_monety(price)
         if result[2] == True:
-            self.print_ticket()
+            reszta = result[1]
+            self.print_ticket(reszta)
         
     
     def select_ulgowy_bilet(self):
@@ -136,7 +73,8 @@ class GUI():
         #obsluga monet
         result = self.sum_monety(price)
         if result[2] == True:
-            self.print_ticket()
+            reszta = result[1]
+            self.print_ticket(reszta)
         
     #funkcja sum 
     def sum_monety(self,price):
@@ -146,10 +84,11 @@ class GUI():
             sum += num
             if sum >= price:
                 if sum > price:
-                    print(f'Wrzucona ilosc: {sum}, Reszta do wydania: ')
+                    print(f'Wrzucona ilosc: {sum}, Reszta do wydania: {sum - price}')
                 return sum, sum - price, True
             else:
                 print(f'Pozostało do wrzucenia: {price-sum} zł')
+
     def cancel_purchase(self):
         self.clear_window()
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Zakup został anulowany", width=300, height=50, font=("Arial", 16), corner_radius=10, text_color="#000000")
@@ -158,19 +97,27 @@ class GUI():
         #dodanie ze po 5 sekundach sie zamyka program
         self.ticket_machine.after(5000, self.ticket_machine.destroy)
 
-    def print_ticket(self):
+    def print_ticket(self, reszta):
         #wydruk biletu
         self.clear_window()
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Wydruk biletu",
                                                       width=300, height=50, font=("Arial", 16,"bold"), corner_radius=10,
                                                       text_color="#000000")
-        self.selection_label.grid(row=0, column=0, columnspan=2, pady=20)
+        self.selection_label.grid(row=0, column=0, columnspan=2, pady=10)
         
-        time.sleep(3)
+
         self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="W trakcie...",
                                                       width=300, height=50, font=("Arial", 16, "bold"),
                                                       corner_radius=10, text_color="#000000")
-        self.selection_label.grid(row=1, column=0, columnspan=2, pady=20)
+        self.selection_label.grid(row=1, column=0, columnspan=2, pady=10)
+
+
+        self.selection_label = customtkinter.CTkLabel(self.ticket_machine, text="Wydawanie reszty "+str(reszta)+"zł",
+                                                      width=300, height=50, font=("Arial", 16, "bold"),
+                                                      corner_radius=10, text_color="#000000")
+        self.selection_label.grid(row=2, column=0, columnspan=2, pady=10)
+
+
             
     def clear_window(self):
         for widget in self.ticket_machine.winfo_children():
